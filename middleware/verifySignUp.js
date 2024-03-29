7e170ee3028af94e133a38033c9f25dbe4902f14
raparisonbryan <raparisonbryan@yahoo.fr>
2024-03-29 16:08:20 +0100

    clean code

--- middleware/verifySignUp.js
+++ middleware/verifySignUp.js
@@ -2,13 +2,11 @@ const userModel = require("../Models/UserModel");
 
 const checkDuplicateUsernameOrEmail = async (req, res, next) => {
   try {
-    // Vérification du nom d'utilisateur
     const userByName = await userModel.findOne({ username: req.body.name });
     if (userByName) {
       return res.status(400).send({ message: "Nom d'utilisateur déjà utilisé" });
     }
 
-    // Vérification de l'email
     const userByEmail = await userModel.findOne({ email: req.body.email });
     if (userByEmail) {
       return res.status(400).send({ message: "Email déjà utilisé" });
